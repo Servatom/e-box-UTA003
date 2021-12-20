b68fdc304ee73e6e30d19ec4335f323eba12cfed
Uddeepta Raaj Kashyap <96105929+okieLoki@users.noreply.github.com>
2021-12-21 01:25:03 +0530

    Update 12.c
    changed line 8, it should be j=n

--- Conditional statements and Loops/iDesign/12.c
+++ Conditional statements and Loops/iDesign/12.c
@@ -5,7 +5,7 @@ int main(){
     int n;
     scanf("%d", &n);
     for(int i=1; i<=n;i++){
-        for(int j=4; j>i; j--){
+        for(int j=n; j>i; j--){
             printf(" ");
         }
         for(int k=1; k<=i; k++){
